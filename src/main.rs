f67b3663cb177a2f093615c75d81e925b9005b0b
vulpidev <pjadasz07@gmail.com>
2023-11-19 19:58:17 +0100

    Checking for required files | v. 0.11.0a
    Added config module and function in main which checks if required files exist.
    
    Config module:
    
    - FileChecker is used for checking if a file exists, or if it's empty.
    - create_config_file is used to create a config file with each String in Vec<String> as a new line in the config.txt.
    
    Created tests module for future testing.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,56 @@
-use std::{env::{self, ArgsOs}, ffi::OsString,};
+mod config;
+pub mod tests;
+use std::{env::{self, ArgsOs}, ffi::OsString, fs::File, io::{self},};
+use config::FileChecker;
+
 fn main() {
+    check_and_fix_required_files();
+    
     let command_arguments = env::args_os();
     let string_array = get_string_arguments(command_arguments);
+}
+
+fn ask_user_for_string_data(message: &str) -> String{
+    println!("{}", message);
+    let mut data = String::new();
+    match io::stdin().read_line(&mut data){
+        Ok(_) => {},
+        Err(err) => {
+            print!("Error: {}", err);
+            return ask_user_for_string_data(message);
+        }
+    }
+    return data;
+}
+
+fn check_and_fix_required_files() {
+    if FileChecker::is_empty("config.txt") {
+        println!("Config file appears to be missing, or is empty.\n--- IF this is your first time launching this app - DON'T WORRY! ---");
+        let username = ask_user_for_string_data("Please enter your username.");
+        let email = ask_user_for_string_data("Please enter your email.");
+        match config::create_config_file(vec![username, email]){
+            Ok(_) => {},
+            Err(e) =>{
+                println!("An error occured while creating a config file, please try again.\nError message: {}", e);
+                check_and_fix_required_files();
+            }
+        }
+    }
+
+    if !FileChecker::file_exists("vault.json"){
+        println!("Creating vault file.");
+        match File::create("vault.json") {
+            Ok(_) => {},
+            Err(e) => {
+                println!("An error occured while creating the vault file. Trying again... \nError message: {}", e);
+                check_and_fix_required_files();
+            }
+        }
+    }
+
+    if FileChecker::is_empty("key.txt"){
+        println!("Key file appears to be missing, or is empty.");
+    }
 }
 
 fn get_string_arguments(args_os: ArgsOs) -> Vec<String>{
